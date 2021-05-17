66691d4f092128db1532fc4a228315ad7d359b3f
yohnc <yohnc@DESKTOP-MJ92OLO>
2021-05-17 17:18:09 -0500

    Add project files.

---
+++ Project1.cpp
@@ -0,0 +1,18 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int favoriteNumber;
+    cout << "Enter your favorite number between 1 and 100: \n";
+    cin >> favoriteNumber;
+    cout << "Amazing "<< favoriteNumber << " is my favorite number too!" << endl;
+    return 0;
+}
+
